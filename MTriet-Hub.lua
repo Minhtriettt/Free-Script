2f97bb03a3ea9a52638f6948e6cd40efecf94577
Hmtriet <140694662+Minhtriettt@users.noreply.github.com>
2025-01-21 02:10:54 -1200

    Update MTriet-Hub.lua

--- MTriet-Hub.lua
+++ MTriet-Hub.lua
@@ -1,1 +1,7 @@
-print("Wait me update")
+    game.StarterGui:SetCore("SendNotification", {
+    Title = "MTriet Hub", 
+    Text = "Wait For Update Pls....",
+    Duration = 3
+})
+wait(.3)
+game.Players.LocalPlayer:Kick("MTriet Hub | Script Updating...")
