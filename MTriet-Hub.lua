24ef07058c3e228a1078511d47cbfcdb57447c01
Hmtriet <140694662+Minhtriettt@users.noreply.github.com>
2025-02-14 02:15:54 -1200

    Delete MTriet-Hub.lua

--- MTriet-Hub.lua
+++
@@ -1,7 +0,0 @@
-    game.StarterGui:SetCore("SendNotification", {
-    Title = "MTriet Hub", 
-    Text = "Wait For Update Pls....",
-    Duration = 3
-})
-wait(.3)
-game.Players.LocalPlayer:Kick("MTriet Hub | discord.gg/mFzWdBUn45")
